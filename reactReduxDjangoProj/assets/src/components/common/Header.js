78966af5f7db0154f89851e98ebb838ae11a6f84
Feng Shang <fshang@apple.com>
2017-06-29 17:20:05 -0700

    react routes code cleanup

--- reactReduxDjangoProj/assets/src/components/common/Header.js
+++ reactReduxDjangoProj/assets/src/components/common/Header.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Route
-} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
   return (
