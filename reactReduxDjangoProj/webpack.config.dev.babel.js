64cd6b2e3e125df501570878d8632f43e0323a52
Feng Shang <fshang@apple.com>
2017-07-21 11:51:20 -0700

    ignore files

--- reactReduxDjangoProj/webpack.config.dev.babel.js
+++ reactReduxDjangoProj/webpack.config.dev.babel.js
@@ -13,8 +13,12 @@ config.plugins = config.plugins.concat([
 ])
 
 config.module.loaders.push(
-  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader?sourceMap'] },
-  {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']}
+  {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader?sourceMap']},
+  {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']},
+  {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+  {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
+  {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+  {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
 )
 
 export default config;
